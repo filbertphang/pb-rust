1f182cfa6af5f4f4359f4f6c6d88a01a1b800955
filbert <fillberth@gmail.com>
2024-09-25 15:16:10 +0800

    fix equality check in # signatures received

--- src/protocol.rs
+++ src/protocol.rs
@@ -27,6 +27,7 @@ pub enum Message<V> {
     },
     Echo {
         round: Round,
+        // PartialSignature (from the paper) or LightSignature (from the source code)?
         partial_signature: PartialSignature,
     },
 }
@@ -51,8 +52,17 @@ pub struct NodeState<V> {
     echoed: HashMap<(Address, Round), (V, Proof)>,
 }
 
+// aside: there are 2 possible designs for the PB interface here.
+// 1) (current impl) implement PB parameters as a struct, and implement PB functions as an impl of that struct
+// 2) implement PB parameters as a trait, and implement PB functions as normal functions that take in some
+//    `ProvableBroadcast<V>` instead of `self`.
+//    note that this impl is slightly awkward because you can't have variables (like `node_addresses` or
+//    `num_byzantine`) in traits, so we would need to store those as getter functions.`
+// i haven't referenced the API design in other protocol crates yet, but i'll take a look soon.
+
 // <V> refers to the type of the Value that we want to reach a consensus on.
-// TODO: might want to parameterize over Proof type too. idk
+// TODO: might want to parameterize over Proof type too.
+// (as well as other type parameters like PartialSignature, CombinedSignature, etc)
 pub struct ProvableBroadcast<V> {
     node_addresses: Vec<Address>,
     num_byzantine: usize,
@@ -125,8 +135,6 @@ impl<V: Clone> ProvableBroadcast<V> {
     }
 
     fn proc_msg(&self, st: &mut NodeState<V>, src: Address, msg: Message<V>) -> Vec<Packet<V>> {
-        // TODO: there's some other `routine_check` thing being done in the actual implementation.
-        // investigate what that's for.
         let NodeState {
             address,
             sent,
@@ -186,13 +194,14 @@ impl<V: Clone> ProvableBroadcast<V> {
                                 false => {
                                     partial_signatures.push((src, partial_signature));
 
+                                    // (this section is an inlined version of `routine_check`.)
                                     // check if we should combine signatures
-                                    let has_enough_signatures =
-                                        partial_signatures.len() >= self.signature_threshold();
+                                    let has_exactly_enough_signatures =
+                                        partial_signatures.len() == self.signature_threshold();
                                     let has_combined_signature = output.contains_key(&round);
 
                                     // combine signatures, if possible
-                                    if has_enough_signatures && !has_combined_signature {
+                                    if has_exactly_enough_signatures && !has_combined_signature {
                                         let partial_signatures_only =
                                             partial_signatures.iter().map(|(_, x)| x).collect();
                                         let combined_signature = (self.combine_partial_signatures)(
