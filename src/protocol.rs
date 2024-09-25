c46688253509922ea3f5a0111fc19aec3e54bfa5
filbert <fillberth@gmail.com>
2024-09-25 17:05:21 +0800

    parameterize over round and proof type

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,9 +1,9 @@
 use std::collections::{HashMap, HashSet};
 
 type Address = i32;
-type Round = i32;
 
 //  partial signature is the same as LightSignature in the actual code.
+//  TODO: will probably have to parameterize over partial and combined signature type too.
 type PartialSignature = ();
 type CombinedSignature = ();
 
@@ -12,76 +12,78 @@ type CombinedSignature = ();
 // to check that <proof> is a valid proof that <value> was sent by <sender>.
 // (note: original defn on bythos paper doesn't include sender, but assumes
 // sender's public key is known. this is probably because they use address as PK.)
-type Proof = ();
 
-pub enum InternalEvent {
-    SendAction { round: Round },
+pub enum InternalEvent<R> {
+    SendAction { round: R },
 }
 
 #[derive(Clone)]
-pub enum Message<V> {
+pub enum Message<R, V, P> {
     Init {
-        round: Round,
+        round: R,
         value: V,
-        proof: Proof,
+        proof: P,
     },
     Echo {
-        round: Round,
+        round: R,
         // PartialSignature (from the paper) or LightSignature (from the source code)?
         partial_signature: PartialSignature,
     },
 }
 
-pub struct Packet<V> {
+pub struct Packet<R, V, P> {
     src: Address,
     dst: Address,
-    msg: Message<V>,
+    msg: Message<R, V, P>,
     received: bool,
 }
 
-pub struct NodeState<V> {
+pub struct NodeState<R, V, P> {
     address: Address,
     // sender state
     // if Round in Set, then this node is the sender for that round.
-    sent: HashSet<Round>,
+    sent: HashSet<R>,
     // TODO: might want to use a second hash map for this (HashMap<Address, PartialSignature>)
-    counter: HashMap<Round, Vec<(Address, PartialSignature)>>,
-    output: HashMap<Round, CombinedSignature>,
+    counter: HashMap<R, Vec<(Address, PartialSignature)>>,
+    output: HashMap<R, CombinedSignature>,
     // receiver state
-    // (Address, Round) means we echoed a message from Address at Round.
-    echoed: HashMap<(Address, Round), (V, Proof)>,
+    // (Address, Round) means we echoed a message from Address at R.
+    echoed: HashMap<(Address, R), (V, P)>,
 }
 
 // aside: there are 2 possible designs for the PB interface here.
 // 1) (current impl) implement PB parameters as a struct, and implement PB functions as an impl of that struct
 // 2) implement PB parameters as a trait, and implement PB functions as normal functions that take in some
-//    `ProvableBroadcast<V>` instead of `self`.
+//    `ProvableBroadcast<R,V,P>` instead of `self`.
 //    note that this impl is slightly awkward because you can't have variables (like `node_addresses` or
 //    `num_byzantine`) in traits, so we would need to store those as getter functions.`
 // i haven't referenced the API design in other protocol crates yet, but i'll take a look soon.
 
-// <V> refers to the type of the Value that we want to reach a consensus on.
-// TODO: might want to parameterize over Proof type too.
-// (as well as other type parameters like PartialSignature, CombinedSignature, etc)
-pub struct ProvableBroadcast<V> {
+// The protocol is parameterized over the round type R, value type V, and proof type P.
+pub struct ProvableBroadcast<R, V, P> {
     node_addresses: Vec<Address>,
     num_byzantine: usize,
     // generates some form of value and proof
-    value_bft: fn(&Address, &Round) -> (V, Proof),
+    value_bft: fn(&Address, &R) -> (V, P),
     // validate a partial signature
-    externally_validate: fn(&Round, &V, &Proof) -> bool,
+    externally_validate: fn(&R, &V, &P) -> bool,
     // TODO: need to add in a parameter for the private key, probably.
-    partially_sign: fn(&Address, &Round, &V) -> PartialSignature,
-    partially_validate: fn(&Address, &Round, &V, &PartialSignature) -> bool,
+    partially_sign: fn(&Address, &R, &V) -> PartialSignature,
+    partially_validate: fn(&Address, &R, &V, &PartialSignature) -> bool,
     combine_partial_signatures: fn(Vec<&PartialSignature>) -> CombinedSignature,
 }
 
-impl<V: Clone> ProvableBroadcast<V> {
+impl<R, V, P> ProvableBroadcast<R, V, P>
+where
+    R: Clone + Eq + std::hash::Hash,
+    V: Clone,
+    P: Clone,
+{
     fn signature_threshold(&self) -> usize {
         self.node_addresses.len() - self.num_byzantine
     }
 
-    fn make_packet(src: &Address, dst: &Address, msg: &Message<V>) -> Packet<V> {
+    fn make_packet(src: &Address, dst: &Address, msg: &Message<R, V, P>) -> Packet<R, V, P> {
         Packet {
             src: src.clone(),
             dst: dst.clone(),
@@ -91,10 +93,10 @@ impl<V: Clone> ProvableBroadcast<V> {
     }
     // broadcasts a message to all nodes.
     // filbs: for simplicity, will just clone everything first.
-    fn broadcast(&self, src: &Address, msg: Message<V>) -> Vec<Packet<V>> {
+    fn broadcast(&self, src: &Address, msg: Message<R, V, P>) -> Vec<Packet<R, V, P>> {
         self.node_addresses
             .iter()
-            .map(|dst| ProvableBroadcast::<V>::make_packet(src, dst, &msg))
+            .map(|dst| ProvableBroadcast::<R, V, P>::make_packet(src, dst, &msg))
             .collect()
     }
 
@@ -103,7 +105,11 @@ impl<V: Clone> ProvableBroadcast<V> {
     // send the Init message to other nodes.
     // (i think the Coq implementation returns NodeState because its functional.
     // but since rust isn't, we can just update the hashmaps/sets directly.)
-    fn proc_int(&self, st: &mut NodeState<V>, internal_event: InternalEvent) -> Vec<Packet<V>> {
+    fn proc_int(
+        &self,
+        st: &mut NodeState<R, V, P>,
+        internal_event: InternalEvent<R>,
+    ) -> Vec<Packet<R, V, P>> {
         let NodeState {
             address: id,
             sent,
@@ -121,7 +127,7 @@ impl<V: Clone> ProvableBroadcast<V> {
                 // this node has not yet initiated this broadcast.
                 None => {
                     // mark this node as the sender for this round
-                    sent.insert(r);
+                    sent.insert(r.clone());
                     let (v, p) = (self.value_bft)(id, &r);
                     let init_msg = Message::Init {
                         round: r,
@@ -134,7 +140,12 @@ impl<V: Clone> ProvableBroadcast<V> {
         }
     }
 
-    fn proc_msg(&self, st: &mut NodeState<V>, src: Address, msg: Message<V>) -> Vec<Packet<V>> {
+    fn proc_msg(
+        &self,
+        st: &mut NodeState<R, V, P>,
+        src: Address,
+        msg: Message<R, V, P>,
+    ) -> Vec<Packet<R, V, P>> {
         let NodeState {
             address,
             sent,
@@ -147,7 +158,7 @@ impl<V: Clone> ProvableBroadcast<V> {
                 round,
                 value,
                 proof,
-            } => match (sent.get(address), echoed.get(&(*address, round))) {
+            } => match (sent.get(&round), echoed.get(&(*address, round.clone()))) {
                 (None, None) => {
                     // we are not the sender, AND we haven't echoed this message before.
                     // so, we should echo this message.
@@ -159,14 +170,15 @@ impl<V: Clone> ProvableBroadcast<V> {
                             let partial_signature = (self.partially_sign)(&address, &round, &value);
 
                             // update `echoed` map
-                            echoed.insert((src, round), (value, proof));
+                            echoed.insert((src, round.clone()), (value, proof));
 
                             // construct message and convert to packets
-                            let msg = Message::Echo::<V> {
+                            let msg = Message::Echo::<R, V, P> {
                                 round,
                                 partial_signature,
                             };
-                            let packet = ProvableBroadcast::<V>::make_packet(&address, &src, &msg);
+                            let packet =
+                                ProvableBroadcast::<R, V, P>::make_packet(&address, &src, &msg);
                             vec![packet]
                         }
                         // could not validate sender's signature: don't echo.
@@ -183,13 +195,14 @@ impl<V: Clone> ProvableBroadcast<V> {
             Message::Echo {
                 round,
                 partial_signature,
-            } => match (sent.get(address), output.get(&round)) {
+            } => match (sent.get(&round), output.get(&round)) {
                 // is the sender AND we haven't made a partial signature yet.
                 (Some(_), None) => {
                     let (value, _) = (self.value_bft)(&address, &round);
                     match (self.partially_validate)(&address, &round, &value, &partial_signature) {
                         true => {
-                            let partial_signatures = counter.entry(round).or_insert(Vec::new());
+                            let partial_signatures =
+                                counter.entry(round.clone()).or_insert(Vec::new());
                             match partial_signatures.contains(&(src, partial_signature)) {
                                 false => {
                                     partial_signatures.push((src, partial_signature));
